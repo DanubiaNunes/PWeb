d0c49717c8f657b723e91d87912fbfbeaa04ac5d
Danubia Silva <danubia.nunes.s@gmail.com>
2021-04-14 21:28:14 -0300

    Atividade 08 de pweb

---
+++ Atividade8/index.js
@@ -0,0 +1,13 @@
+var Aluno1 = new Object();
+Aluno1.ra = '0030481913007';
+Aluno1.nome = 'Danubia Silva';
+
+var Aluno1 = {};
+Aluno1.ra = '0030481913007';
+Aluno1.nome = 'Danubia Silva';
+
+var Aluno1 = {
+    ra: '0030481913007',
+    nome: 'Danubia Silva'
+};
+//alert("teste " + Aluno1.ra + "\n" + Aluno1.nome);
