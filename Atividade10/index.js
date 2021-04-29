f4080cbbbb0b6931fe8ad6460aa04865e970d8e2
Danubia Silva <danubia.nunes.s@gmail.com>
2021-04-28 21:47:23 -0300

    Atividade 10 de pweb

---
+++ Atividade10/index.js
@@ -0,0 +1,11 @@
+function mouseEmCima(obj) {
+    obj.src = 'janelaaberta.png';
+}
+
+function mouseFora(obj) {
+    obj.src = 'janelaFechada.png';
+}
+
+function mouseClick(obj) {
+    obj.src = 'janelaquebra.png';
+}
